04e55653a56975c78a200aed34a3687094a95e3b
Hadieee <hadiepratama15@gmail.com>
2023-11-16 10:25:48 +0800

    Update: Gambar untuk intro

--- pa_mobile/lib/screen/main.dart
+++ pa_mobile/lib/screen/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pa_mobile/intro.dart';
 import 'package:pa_mobile/provider/change_theme.dart';
+import 'package:pa_mobile/screen/admin/home_admin.dart';
+import 'package:pa_mobile/screen/user/home.dart';
+import 'package:pa_mobile/services/user_service.dart';
 import 'package:provider/provider.dart';
 
 //disini bakal ada pengecekan preference apakah pernah login atau tidak =>database
@@ -22,8 +25,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: Provider.of<CustomTheme>(context).currentTheme,
-      home: IntroductionPage(),
-    );
+        theme: Provider.of<CustomTheme>(context).currentTheme,
+        home: FutureBuilder(
+            future: userservice().uservalue(),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  if (snapshot.data["status"] == "admin") {
+                    return home_admin();
+                  } else if (snapshot.data["status"] == "user") {
+                    return home();
+                  }
+                } else {
+                  return IntroductionPage(); //ganti jadi intro
+                }
+              }
+              return Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            }));
   }
 }
