4a2f56e253d62177e8e68302ee053bca61c98521
Hadieee <hadiepratama15@gmail.com>
2023-11-27 23:21:56 +0800

    Update: Fix import

--- pa_mobile/lib/screen/user/home.dart
+++ pa_mobile/lib/screen/user/home.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
